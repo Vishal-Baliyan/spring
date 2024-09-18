48c11e50fa18fa092088de9addf8fcc50ff52ab5
unknown <vishalbaliyan488@gmail.com>
2024-09-18 22:24:44 +0530

    first commit

---
+++ src/main/java/com/javatechie/service/ProductService.java
@@ -0,0 +1,50 @@
+package com.javatechie.service;
+
+import com.javatechie.dto.Product;
+import com.javatechie.entity.UserInfo;
+import com.javatechie.repository.UserInfoRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Service
+public class ProductService {
+
+    List<Product> productList = null;
+
+    @Autowired
+    private UserInfoRepository repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+//    @PostConstruct
+//    public void loadProductsFromDB() {
+//         productList = repository.findAll();
+//    } 
+
+
+    public List<Product> getProducts() {
+        return productList;
+    }
+
+    public Product getProduct(int id) {
+        return productList.stream()
+                .filter(product -> product.getProductId() == id)
+                .findAny()
+                .orElseThrow(() -> new RuntimeException("product " + id + " not found"));
+    }
+
+
+    public String addUser(UserInfo userInfo) {
+        userInfo.setPassword(passwordEncoder.encode(userInfo.getPassword()));
+        repository.save(userInfo);
+        return "user added to system ";
+    }
+}
